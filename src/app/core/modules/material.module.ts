a016733a25a5a9e46dfa0b764232f142010da6e6
Julia-Kopichenko <j.titorenko@gmail.com>
2023-04-21 17:38:37 +0200

    feat: variables, mixins, theme

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+const materialModules = [MatButtonModule];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-  ],
-  exports: [],
+  imports: [CommonModule, ...materialModules],
+  exports: [...materialModules],
 })
-export default class MaterialModule { }
+export default class MaterialModule {}
