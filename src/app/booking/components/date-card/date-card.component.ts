d6e9e007f194a302d9d2c610c853f0ccfbd11253
Alexey Kuptsov <jerubbaal@mail.ru>
2023-05-05 17:27:12 +0600

    feat: add date block

--- src/app/booking/components/date-card/date-card.component.ts
+++ src/app/booking/components/date-card/date-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Price } from '../../models/price.model';
 
 @Component({
   selector: 'app-date-card',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./date-card.component.scss']
 })
 export class DateCardComponent {
+  @Input() price?: Price | null;
 
+  @Input() date?: Date;
+
+  @Input() isSellected?: boolean;
 }
--- src/app/booking/components/top-date-cards/top-date-cards.component.ts
+++ src/app/booking/components/top-date-cards/top-date-cards.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PriceList } from '../../models/prices.model';
+
+const DAY_IN_MILISECONDS = 1000 * 60 * 60 * 24;
 
 @Component({
   selector: 'app-top-date-cards',
   templateUrl: './top-date-cards.component.html',
   styleUrls: ['./top-date-cards.component.scss']
 })
-export class TopDateCardsComponent {
+export class TopDateCardsComponent implements OnInit {
   @Input() date?: Date;
 
   @Input() prices?: PriceList;
 
   @Input() avaible?: number;
+
+  twoDaysBefore?: Date;
+
+  oneDayBefore?: Date;
+
+  oneDayAfter?: Date;
+
+  twoDaysAfter?: Date;
+
+  ngOnInit(): void {
+    this.twoDaysBefore = new Date(this.date?.getTime() ?? 0 - DAY_IN_MILISECONDS * 2);
+    this.oneDayBefore = new Date(this.date?.getTime() ?? 0 - DAY_IN_MILISECONDS);
+    this.oneDayAfter = new Date(this.date?.getTime() ?? 0 + DAY_IN_MILISECONDS);
+    this.twoDaysAfter = new Date(this.date?.getTime() ?? 0 + DAY_IN_MILISECONDS * 2);
+  }
 }
